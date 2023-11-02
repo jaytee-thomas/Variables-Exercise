45f45e677eba61637280b7fce5de6f1b154a8e1b
Jaytee-Thomas <jt.aws.cloud@protonmail.com>
2023-11-02 16:01:20 -0500

    assignment completed finally

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Variable Assignment
+
+            string myName = "Jason Thomas";
+            int myAge = 49;
+            char middleInitial = 'S'; // Corrected the variable name and assigned a character 'S'
+            bool isSingle = true; // Corrected the variable name
+            double randomDbl = 29.292929;
+            decimal randomDec = 3.3m; // Added 'm' to specify that it's a decimal
+
+            Console.WriteLine($"Hello, my name is {myName}, and I am {myAge} years old, my middle initial is {middleInitial}, marital status single {isSingle}, random number {randomDbl}, and decimal {randomDec}");
         }
     }
 }
